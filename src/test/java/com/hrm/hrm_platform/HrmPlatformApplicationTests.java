3d63e326688bbf18b866935bcdc0f23c37cfbdc8
Victoria Kharlanchuk <kaplabetty@gmail.com>
2024-05-17 13:32:11 +0200

    first commit

---
+++ src/test/java/com/hrm/hrm_platform/HrmPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrm.hrm_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
