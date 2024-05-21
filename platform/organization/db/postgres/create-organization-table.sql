8605ce50fb782b9741aa7b413c02c187a858e2b2
Victoria Kharlanchuk <kaplabetty@gmail.com>
2024-05-22 00:12:21 +0200

    3 : create organization table - got fail- should to figure out

---
+++ platform/organization/db/postgres/create-organization-table.sql
@@ -0,0 +1,11 @@
+-- organization table
+
+CREATE TABLE IF NOT EXISTS public.organizations
+(
+                               id varchar NOT NULL,
+                               name VARCHAR(255) NOT NULL,
+                               address VARCHAR(255) NOT NULL,
+                               contact_information VARCHAR(255),
+                               description TEXT,
+    CONSTRAINT organization_pk PRIMARY KEY (id)
+);
