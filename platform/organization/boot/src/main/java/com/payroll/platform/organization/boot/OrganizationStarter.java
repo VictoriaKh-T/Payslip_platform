b089aebb5a2d46efbcb4eacb605f939b03f9d7e0
Victoria Kharlanchuk <kaplabetty@gmail.com>
2024-08-12 20:55:48 +0200

    [BOOT] starter rename

--- platform/organization/boot/src/main/java/com/payroll/platform/organization/boot/OrganizationStarter.java
+++ platform/organization/boot/src/main/java/com/payroll/platform/organization/boot/OrganizationStarter.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrgStarter {
+public class OrganizationStarter {
     public static void main(String[] args) {
-        SpringApplication.run(OrgStarter.class, args);
+        SpringApplication.run(OrganizationStarter.class, args);
     }
 }
