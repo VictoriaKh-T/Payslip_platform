6f1e6ab1df074646fce753d9f47d3ca7849120b5
Victoria Kharlanchuk <kaplabetty@gmail.com>
2024-08-12 20:54:41 +0200

    [BOOT] starter

--- platform/organization/boot/src/main/java/com/payroll/platform/organization/boot/OrgStarter.java
+++ platform/organization/boot/src/main/java/com/payroll/platform/organization/boot/OrgStarter.java
@@ -1,3 +1,11 @@
 package com.payroll.platform.organization.boot;
 
-public class OrgStarter {}
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrgStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(OrgStarter.class, args);
+    }
+}
