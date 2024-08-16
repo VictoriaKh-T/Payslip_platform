40d077dd97ba6b4fc2c935b13ebec6b3f77d9cf4
Victoria Kharlanchuk <kaplabetty@gmail.com>
2024-08-16 23:35:11 +0200

    [INFRA] Controller; banner; properties

--- platform/organization/infra/src/main/java/com/payroll/platform/orginfra/adapter/driving/controller/OrganizationController.java
+++ platform/organization/infra/src/main/java/com/payroll/platform/orginfra/adapter/driving/controller/OrganizationController.java
@@ -1,5 +1,10 @@
 package com.payroll.platform.orginfra.adapter.driving.controller;
 
+import com.payroll.platform.orgapp.port.in.CreateOrganizationUseCase;
+import com.payroll.platform.orgapp.port.in.DeleteOrganizationUseCase;
+import com.payroll.platform.orgapp.port.in.FindOrganizationByKodUseCase;
+import com.payroll.platform.orgapp.port.in.FindOrganizationByOrganizationIdUseCase;
+import com.payroll.platform.orgapp.port.in.UpdateOrganizationByIdUseCase;
 import com.payroll.platform.orgdomain.dto.OrganizationId;
 import com.payroll.platform.orgdomain.dto.OrganizationRequest;
 import com.payroll.platform.orgdomain.dto.OrganizationResponse;
@@ -24,13 +29,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/organizations")
 public class OrganizationController {
-  private final OrganizationAdapter adapter;
+  private final FindOrganizationByOrganizationIdUseCase findOrganizationByOrganizationId;
+  private final FindOrganizationByKodUseCase findOrganizationByKod;
+  private final UpdateOrganizationByIdUseCase updateOrganizationById;
+  private final DeleteOrganizationUseCase deleteOrganization;
+  private final CreateOrganizationUseCase createOrganization;
 
   @GetMapping("/{id}")
   @Tag(name = "Get organization", description = "This endpoint get an organization by ID")
   public OrganizationResponse findOrganizationById(@PathVariable Long id) {
-    return adapter.findByOrganizationId(new OrganizationId(id));
-    // change to Long id instant of OrganizationId
+    return findOrganizationByOrganizationId.findOrganizationByIdUseCase(id);
   }
 
   @GetMapping("{/kod}")
@@ -38,24 +46,24 @@ public class OrganizationController {
       name = "Get organization",
       description = "This endpoint get an organization by organization_kod")
   public OrganizationResponse findOrganizationByKod(@PathVariable String kod) {
-    return adapter.findByOrganizationKod(kod);
+    return findOrganizationByKod.findOrganizationByKodUseCase(kod);
   }
 
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
   @Tag(name = "Create new organization", description = "This endpoint create an organization")
   public OrganizationResponse createOrganization(@RequestBody OrganizationRequest request) {
-    return adapter.addOrganization(request);
+    return createOrganization.addOrganization(request);
   }
 
   @PutMapping("{/id}")
   public UpdateOrganizationResponse updateOrganization(
       @PathVariable Long id, @RequestBody UpdateOrganizationRequest request) {
-    return adapter.updateOrganizationById(id, request);
+    return updateOrganizationById.updateOrganization(id, request);
   }
 
   @DeleteMapping("{/id}")
   public void deleteOrganization(@PathVariable Long id) {
-    adapter.deleteOrganizationById(new OrganizationId(id));
+    deleteOrganization.deleteOrganizationById(new OrganizationId(id));
   }
 }
