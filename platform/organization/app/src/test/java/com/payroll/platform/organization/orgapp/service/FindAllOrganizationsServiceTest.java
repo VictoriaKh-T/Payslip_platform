81f4ce59e9c90a1119b28e3bccbb76d6a7399f35
Victoria Kharlanchuk <kaplabetty@gmail.com>
2024-09-20 23:15:47 +0200

    checkstyle

--- platform/organization/app/src/test/java/com/payroll/platform/organization/orgapp/service/FindAllOrganizationsServiceTest.java
+++ platform/organization/app/src/test/java/com/payroll/platform/organization/orgapp/service/FindAllOrganizationsServiceTest.java
@@ -1,14 +1,5 @@
 package com.payroll.platform.organization.orgapp.service;
 
-import com.payroll.platform.organization.orgapp.port.out.persistence.OrganizationRepository;
-import com.payroll.platform.organization.orgdomain.dto.OrganizationResponse;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import java.util.Arrays;
-import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -16,35 +7,47 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.payroll.platform.organization.orgapp.port.out.persistence.OrganizationRepository;
+import com.payroll.platform.organization.orgdomain.dto.OrganizationResponse;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 @ExtendWith(MockitoExtension.class)
 class FindAllOrganizationsServiceTest {
-    @Mock
-    private OrganizationRepository repository;
+  @Mock private OrganizationRepository repository;
 
-    @InjectMocks
-    private  FindAllOrganizationsService service;
+  @InjectMocks private FindAllOrganizationsService service;
 
-    @Test
-    void findAll_ShouldReturnListOfOrganizations() {
-        OrganizationResponse org1 = new OrganizationResponse(1L, "ORG001", "Tech1111", "+380122221133", "123 Main St, Cityville");
-        OrganizationResponse org2 = new OrganizationResponse(2L, "ORG002", "BizCorp", "+380122221144", "456 Market St, Townsville");
+  @Test
+  void findAll_ShouldReturnListOfOrganizations() {
+    OrganizationResponse org1 =
+        new OrganizationResponse(
+            1L, "ORG001", "Tech1111", "+380122221133", "123 Main St, Cityville");
+    OrganizationResponse org2 =
+        new OrganizationResponse(
+            2L, "ORG002", "BizCorp", "+380122221144", "456 Market St, Townsville");
 
-        List<OrganizationResponse> expectedOrganizations = Arrays.asList(org1, org2);
+    List<OrganizationResponse> expectedOrganizations = Arrays.asList(org1, org2);
 
-        when(repository.findAllOrganizations()).thenReturn(expectedOrganizations);
-        List<OrganizationResponse> actualOrganizations = service.findAll();
-        assertNotNull(actualOrganizations);
-        assertEquals(2, actualOrganizations.size());
-        assertEquals(expectedOrganizations, actualOrganizations);
-        verify(repository, times(1)).findAllOrganizations();
-    }
+    when(repository.findAllOrganizations()).thenReturn(expectedOrganizations);
+    List<OrganizationResponse> actualOrganizations = service.findAll();
+    assertNotNull(actualOrganizations);
+    assertEquals(2, actualOrganizations.size());
+    assertEquals(expectedOrganizations, actualOrganizations);
+    verify(repository, times(1)).findAllOrganizations();
+  }
 
-    @Test
-    void findAll_ShouldReturnEmptyList() {
-        when(repository.findAllOrganizations()).thenReturn(Arrays.asList());
-        List<OrganizationResponse> actualOrganizations = service.findAll();
-        assertNotNull(actualOrganizations);
-        assertTrue(actualOrganizations.isEmpty());
-        verify(repository, times(1)).findAllOrganizations();
-    }
+  @Test
+  void findAll_ShouldReturnEmptyList() {
+    when(repository.findAllOrganizations()).thenReturn(Arrays.asList());
+    List<OrganizationResponse> actualOrganizations = service.findAll();
+    assertNotNull(actualOrganizations);
+    assertTrue(actualOrganizations.isEmpty());
+    verify(repository, times(1)).findAllOrganizations();
+  }
 }
--- platform/organization/app/src/test/java/com/payroll/platform/organization/orgapp/service/FindOrganizationByKodServiceTest.java
+++ platform/organization/app/src/test/java/com/payroll/platform/organization/orgapp/service/FindOrganizationByKodServiceTest.java
@@ -1,13 +1,5 @@
 package com.payroll.platform.organization.orgapp.service;
 
-import com.payroll.platform.organization.orgapp.port.out.persistence.OrganizationRepository;
-import com.payroll.platform.organization.orgdomain.dto.FindOrganizationByKodRequest;
-import com.payroll.platform.organization.orgdomain.dto.OrganizationResponse;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -16,49 +8,54 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.payroll.platform.organization.orgapp.port.out.persistence.OrganizationRepository;
+import com.payroll.platform.organization.orgdomain.dto.FindOrganizationByKodRequest;
+import com.payroll.platform.organization.orgdomain.dto.OrganizationResponse;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 @ExtendWith(MockitoExtension.class)
 class FindOrganizationByKodServiceTest {
-    @Mock
-    private OrganizationRepository repository;
+  @Mock private OrganizationRepository repository;
 
-    @InjectMocks
-    private   FindOrganizationByKodService service;
+  @InjectMocks private FindOrganizationByKodService service;
 
-    @Test
-    void findOrganizationByKod_ShouldReturnOrganization() {
-        String validKod = "ORG001";
-        FindOrganizationByKodRequest request = new FindOrganizationByKodRequest(validKod);
-        OrganizationResponse expectedResponse = new OrganizationResponse(
-                1L, validKod, "Tech1111", "+380122221133", "123 Main St, Cityville"
-        );
-        when(repository.findByOrganizationKod(request)).thenReturn(expectedResponse);
-        OrganizationResponse actualResponse = service.findOrganizationByKodUseCase(request);
-        assertNotNull(actualResponse);
-        assertEquals(expectedResponse, actualResponse);
-    }
+  @Test
+  void findOrganizationByKod_ShouldReturnOrganization() {
+    String validKod = "ORG001";
+    FindOrganizationByKodRequest request = new FindOrganizationByKodRequest(validKod);
+    OrganizationResponse expectedResponse =
+        new OrganizationResponse(
+            1L, validKod, "Tech1111", "+380122221133", "123 Main St, Cityville");
+    when(repository.findByOrganizationKod(request)).thenReturn(expectedResponse);
+    OrganizationResponse actualResponse = service.findOrganizationByKodUseCase(request);
+    assertNotNull(actualResponse);
+    assertEquals(expectedResponse, actualResponse);
+  }
 
-    @Test
-    void findOrganizationByKod_ShouldReturnNull() {
+  @Test
+  void findOrganizationByKod_ShouldReturnNull() {
 
-        String invalidKod = "INVALID_KOD";
-        FindOrganizationByKodRequest request = new FindOrganizationByKodRequest(invalidKod);
-        when(repository.findByOrganizationKod(request)).thenReturn(null);
-        OrganizationResponse actualResponse = service.findOrganizationByKodUseCase(request);
-        assertNull(actualResponse);
-        verify(repository, times(1)).findByOrganizationKod(request);
-    }
+    String invalidKod = "INVALID_KOD";
+    FindOrganizationByKodRequest request = new FindOrganizationByKodRequest(invalidKod);
+    when(repository.findByOrganizationKod(request)).thenReturn(null);
+    OrganizationResponse actualResponse = service.findOrganizationByKodUseCase(request);
+    assertNull(actualResponse);
+    verify(repository, times(1)).findByOrganizationKod(request);
+  }
 
-    @Test
-    void findOrganizationByKodUseCase_ShouldThrowException() {
-        String kod = "ORG001";
-        FindOrganizationByKodRequest request = new FindOrganizationByKodRequest(kod);
-        when(repository.findByOrganizationKod(request)).thenThrow(new RuntimeException("Database error"));
-        Exception exception = assertThrows(RuntimeException.class, () ->
-                service.findOrganizationByKodUseCase(request)
-        );
-        assertEquals("Database error", exception.getMessage());
-        verify(repository, times(1)).findByOrganizationKod(request);
-    }
-
-
+  @Test
+  void findOrganizationByKodUseCase_ShouldThrowException() {
+    String kod = "ORG001";
+    FindOrganizationByKodRequest request = new FindOrganizationByKodRequest(kod);
+    when(repository.findByOrganizationKod(request))
+        .thenThrow(new RuntimeException("Database error"));
+    Exception exception =
+        assertThrows(RuntimeException.class, () -> service.findOrganizationByKodUseCase(request));
+    assertEquals("Database error", exception.getMessage());
+    verify(repository, times(1)).findByOrganizationKod(request);
+  }
 }
